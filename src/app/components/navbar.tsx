21f76bcf1202b595c53a3daf2429b1c018fb0e6a
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-07-14 18:28:54 +0530

    initliaize and navbar done

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className='rounded-md flex flex-row justify-between w-full bg-black p-2 items-center'>
+        <span className='text-white hover:text-[#FFDE4D] cursor-pointer p-2 ml-2 font-headers text-2xl'>
+            <Link href='/'>Rishabh.</Link>
+        </span>
+
+        <ul className='flex flex-row text-white p-2 justify-between w-[20%] mr-2'>
+            <li>
+                <Link href='/works' className='hover:underline'>Works</Link>
+            </li>
+            <li>
+                <a href="https://drive.google.com/file/d/1_Oci-76Dwxe2UNV43ymg204bORW_6vKg/view?usp=sharing" target='_blank' className='hover:underline'>Resume</a>
+            </li>
+            <li>
+                <Link href='/contact' className='hover:underline'>Contact Me</Link>
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/app/works/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/navbar'
+
+const Works: React.FC = () => {
+  return (
+    <div className="m-2">
+        <Navbar />
+      Works
+    </div>
+  )
+}
+
+export default Works
