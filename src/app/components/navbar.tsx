998d4851483b3973a0286b8751906574a096ee59
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-07-16 16:30:58 +0530

    Refactor navbar and sidebar components

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,24 +1,36 @@
-import Link from 'next/link'
-import React from 'react'
+"use client"
+
+import Link from "next/link"
+import React from "react"
 
 const Navbar: React.FC = () => {
   return (
-    <nav className='rounded-md flex flex-row justify-between w-full bg-gray-900/90 p-2 items-center'>
-        <span className='text-white hover:text-[#FFDE4D] cursor-pointer p-2 ml-2 font-headers text-2xl'>
-            <Link href='/'>Rishabh.</Link>
-        </span>
+    <nav className="rounded-md z-20 flex flex-row justify-between w-full bg-slate-800 p-2 items-center">
+      <span className="text-white hover:text-[#FFDE4D] cursor-pointer p-2 ml-2 font-headers text-2xl">
+        <Link href="/">Rishabh.</Link>
+      </span>
 
-        <ul className='flex flex-row text-white p-2 justify-between w-[20%] mr-2'>
-            <li>
-                <Link href='/works' className='hover:underline'>Works</Link>
-            </li>
-            <li>
-                <a href="https://drive.google.com/file/d/1_Oci-76Dwxe2UNV43ymg204bORW_6vKg/view?usp=sharing" target='_blank' className='hover:underline'>Resume</a>
-            </li>
-            <li>
-                <Link href='/contact' className='hover:underline'>Contact Me</Link>
-            </li>
-        </ul>
+      <ul className="flex flex-row text-white p-2 justify-between w-[20%] mr-2">
+        <li>
+          <Link href="/works" className="hover:underline">
+            Works
+          </Link>
+        </li>
+        <li>
+          <a
+            href="https://drive.google.com/file/d/1_Oci-76Dwxe2UNV43ymg204bORW_6vKg/view?usp=sharing"
+            target="_blank"
+            className="hover:underline"
+          >
+            Resume
+          </a>
+        </li>
+        <li>
+          <Link href="/contact" className="hover:underline">
+            Contact Me
+          </Link>
+        </li>
+      </ul>
     </nav>
   )
 }
--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -1,32 +1,40 @@
+"use client";
+
 import React from 'react';
-import { FaLinkedin } from 'react-icons/fa';
+import { FaLinkedin, FaGithub, FaInstagram } from 'react-icons/fa';
+import { RiTwitterXFill } from "react-icons/ri";
+import { SiLeetcode } from "react-icons/si";
+import { BackgroundGradient } from './ui/background-gradient';
 
 const SideBar: React.FC = () => {
   return (
+    <BackgroundGradient animate className="rounded-[22px] max-w-sm p-[2px] dark:bg-zinc-900">
     <nav>
         {/* a sidebar for links of socials */}
 
-        <div className='flex flex-col justify-center transition-all items-center gap-2 p-2 rounded-2xl hover:w-[330%]  bg-slate-800'>
-            <a href='https://www.linkedin.com/in/sytem/' target='_blank' className='text-white hover:text-[#FFDE4D] cursor-pointer p-2 font-headers text-2xl flex flex-col'>
+        <div className='flex flex-col justify-center transition-all items-center gap-2 p-2 rounded-2xl bg-slate-800'>
+            <a href='https://www.linkedin.com/in/rishabhguptajs/' target='_blank' className='text-white hover:text-[#FFDE4D] cursor-pointer p-2 font-headers text-2xl flex flex-col'>
                 <FaLinkedin className=' text-blue-600' />
             </a>
-            <a href='https://www.linkedin.com/in/rishabhguptajs/' target='_blank' className='text-white hover:text-[#FFDE4D] cursor-pointer p-2 font-headers text-2xl'>
-                <FaLinkedin className=' text-blue-600' />
+            <hr className='bg-blue-400' />
+            <a href='https://www.github.com/rishabhguptajs/' target='_blank' className='text-white hover:text-[#FFDE4D] cursor-pointer p-2 font-headers text-2xl'>
+                <FaGithub className=' text-gray-200' />
             </a>
-            <a href='https://www.linkedin.com/in/rishabhguptajs/' target='_blank' className='text-white hover:text-[#FFDE4D] cursor-pointer p-2 font-headers text-2xl'>
-                <FaLinkedin className=' text-blue-600' />
+            <hr />
+            <a href='https://www.twitter.com/rishabhguptajs/' target='_blank' className='text-white hover:text-[#FFDE4D] cursor-pointer p-2 font-headers text-2xl'>
+                <RiTwitterXFill className=' text-gray-100' />
             </a>
-            <a href='https://www.linkedin.com/in/rishabhguptajs/' target='_blank' className='text-white hover:text-[#FFDE4D] cursor-pointer p-2 font-headers text-2xl'>
-                <FaLinkedin className=' text-blue-600' />
+            <hr />
+            <a href='https://www.instagram.com/rishabhgupta.js/' target='_blank' className='text-white hover:text-[#FFDE4D] cursor-pointer p-2 font-headers text-2xl'>
+                <FaInstagram className=' text-pink-500' />
             </a>
-            <a href='https://www.linkedin.com/in/rishabhguptajs/' target='_blank' className='text-white hover:text-[#FFDE4D] cursor-pointer p-2 font-headers text-2xl'>
-                <FaLinkedin className=' text-blue-600' />
-            </a>
-            <a href='https://www.linkedin.com/in/rishabhguptajs/' target='_blank' className='text-white hover:text-[#FFDE4D] cursor-pointer p-2 font-headers text-2xl'>
-                <FaLinkedin className=' text-blue-600' />
+            <hr />
+            <a href='https://www.leetcode.com/rishabhguptajs/' target='_blank' className='text-white hover:text-[#FFDE4D] cursor-pointer p-2 font-headers text-2xl'>
+                <SiLeetcode className='text-amber-400' />
             </a>
         </div>
     </nav>
+    </BackgroundGradient>
   )
 }
 
