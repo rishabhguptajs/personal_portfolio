60adff21cb1d587fd83e30ee2e23f1a736b15d8f
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-07-24 23:47:57 +0530

    feat: Add BLAME and Oddyssey projects to workdata.ts

---
+++ src/app/assets/workdata.ts
@@ -0,0 +1,17 @@
+export const Projects = [
+    {
+        name: 'BLAME - A Blogging App',
+        github: 'https://github.com/rishabhguptajs/Blogging-App',
+        live: 'https://blogwithnoworries.vercel.app',
+        desc: 'A Blogging App made with NextJS and TailwindCSS. It has features like authentication, creating, updating and deleting blogs. Also the use of generative AI to generate blog content.',
+        tech: ['ReactJS', 'NodeJS', 'ExpressJS', 'MongoDB', 'TailwindCSS', 'Firebase', 'Axios', 'Framer Motion', 'React Router DOM', 'Vite'],
+        id: 1
+    },
+    {
+        name: 'Oddyssey - An E-commerce App',
+        github: 'https://github.com/rishabhguptajs/Oddyssey---ecommerce-platform',
+        live: 'https://oddyssey-frontend.vercel.app/',
+        desc: 'An E-commerce App made with MERN Stack. It has features like authentication, creating, updating and deleting products, adding to cart and checkout with payments using Stripe.',
+        tech: ['ReactJS', 'NodeJS', 'ExpressJS', 'MongoDB', 'TailwindCSS', 'Axios', 'React Router DOM', 'Stripe'],
+    }
+]
