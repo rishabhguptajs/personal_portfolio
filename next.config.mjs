7fbe1d7fa3ceb9fda4c98d94eda5068befbcb4b5
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-07-26 00:44:42 +0530

    Fix next.config.mjs for next-sitemap integration

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
-import { withSitemap } from 'next-sitemap';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-};
-
-export default withSitemap(nextConfig);
+    reactStrictMode: true,
+  };
+  
+  export default nextConfig;
+  
